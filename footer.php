e9e1414c766239e51073d234f5887a8b4d4aaba1
Daniel Ehniss <bitbucket@depone.eu>
2014-01-03 18:40:29 +0100

    Minor changes, included link to feed and creative commons in the footer.

--- footer.php
+++ footer.php
@@ -3,11 +3,13 @@
 <footer role="contentinfo">
     <nav id="mainnav" >
         <ul>
+            <li class="page-item"><a href="http://danielehniss.de/feed/" title="RSS Feed abonnieren?" >Feed</a></li>
             <?php wp_list_pages('title_li=&exclude=143'); ?>
         </ul>
     </nav>
     <div class="suchfeld"><?php get_search_form(); ?></div>
     <hr />
+    <p class="cc">Lizenz: <a href="http://creativecommons.org/licenses/by-sa/4.0/deed.de" title="Umgangssprachliche Erkl&auml;rung der Lizenz des Inhalts dieses Blogs" target="_blank" >BY-SA Creative Commons</a>, Daniel Ehniss.</p>
 </footer><!-- footer -->
 </section><!-- page -->
 
@@ -15,7 +17,7 @@
 
 <?php if (is_single()) { ?>
         <script src="http://code.jquery.com/jquery-1.7.2.min.js" ></script>
-        <script src="./wp-content/themes/daniel14/js/jquery.oembed.min.js" ></script>
+        <script src="http://danielehniss.de/wp-content/themes/daniel14/js/jquery.oembed.min.js" ></script>
         
         <script>
             if (window.navigator.standalone == false) {
@@ -46,7 +48,7 @@
 
 <?php } else { ?>
 
-<script src="./wp-content/themes/daniel14/js/jquery.oembed.min.js" ></script>
+<script src="http://danielehniss.de/wp-content/themes/daniel14/js/jquery.oembed.min.js" ></script>
         <script type="text/javascript" >
             if (window.navigator.standalone == false) {
                 window.scrollTo(0, 1);
