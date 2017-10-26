7055bcd07a2597cc1e2194d23142ac917f8adb16
Daniel Ehniss <daniel@depone.de>
2017-10-27 00:12:35 +0200

    Add h-entry to post-class

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 		<?php while (have_posts()) : the_post(); ?>
 
-			<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<article <?php post_class('h-entry'); ?> id="post-<?php the_ID(); ?>">
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Link zu %s', ''), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h2>
 
 				<div class="entry">
