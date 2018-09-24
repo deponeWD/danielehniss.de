1e478654a8bc69408bd0c7e0f1d2c71046693250
Daniel Ehniss <daniel@depone.de>
2018-09-24 22:36:19 +0200

    Adapt hcard on index.php

--- index.php
+++ index.php
@@ -18,7 +18,21 @@
 						<?php get_template_part( 'display-content' ); ?>
 					</div><!-- entry -->
 				<?php } ?>
-				<p class="postmetadata">Ver&ouml;ffentlicht am <?php the_time(__('d.m.Y', '')) ?> von <?php the_author() ?> <?php edit_post_link(__('bearbeiten', ''), '(', ') '); ?> </p>
+				<div class="entry-meta hcard h-card">
+					<address class="byline">
+						<span class="author p-author vcard" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+							<?php echo get_avatar( get_the_author_meta( 'ID' ), 100, null, null, array( 'class' => array( 'u-photo' ))); ?>
+							<span class="description">Ver&ouml;ffentlicht von</span>
+							<a class="url uid u-url u-uid fn p-name" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" title="View all posts by <?php the_author(); ?>" rel="author" itemprop="url">
+								<span itemprop="name"><?php the_author(); ?></span>
+							</a>
+						</span>
+					</address>
+					<span class="conjunction">am</span>
+					<a href="<?php the_permalink(); ?>" rel="bookmark" class="url u-url"><time class="entry-date updated published dt-updated dt-published" datetime="<?php the_time(__('c', '')); ?>" itemprop="dateModified datePublished"><?php the_time(__('d.m.Y', '')); ?></time></a>
+					<?php printf(__(' in %s', ''), get_the_category_list(', ')); ?>.
+					<?php edit_post_link(__('bearbeiten', ''), '(', ') '); ?>
+				</div>
 			</article><!-- post -->
 
 		<?php endwhile; ?>
